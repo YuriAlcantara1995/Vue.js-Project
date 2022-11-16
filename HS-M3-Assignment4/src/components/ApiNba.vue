24f56575e6a9b0defc717334f858676df9819cdb
Yuri Alcantara <yuri.alcantara1995@gmail.com>
2022-11-16 21:47:56 +0100

    Choosing new API

--- HS-M3-Assignment4/src/components/ApiNba.vue
+++ HS-M3-Assignment4/src/components/ApiNba.vue
@@ -2,30 +2,23 @@
     import { ref } from 'vue'
 	const response = ref({})
 
-    const url = 'https://nba-stats4.p.rapidapi.com/teams/';
+    const url = 'https://free-nba.p.rapidapi.com/players/';
 
     const options = {
 	method: 'GET',
-	    headers: {
-		    'X-RapidAPI-Key': '0fc6e49d17msh56f6baf2d560196p1e03ecjsnf2c9e7768ecf',
-		    'X-RapidAPI-Host': 'nba-stats4.p.rapidapi.com'
-	    }
-    };
+	headers: {
+		'X-RapidAPI-Key': '0fc6e49d17msh56f6baf2d560196p1e03ecjsnf2c9e7768ecf',
+		'X-RapidAPI-Host': 'free-nba.p.rapidapi.com'
+	}
+};
     
 
-    // adding delay
-    await new Promise(resolve => {
-        setTimeout(() => { 
-            resolve()
-        }, 1000)
-    })
-    
     try {
         const res = await fetch(url, options)
         console.log(res)
         response.value = (await res.json())
     } catch (error) {
-        response.value = { answer: 'Error! Could not reach the API. ' + error, image: '' }
+        response.value = { answer: 'Error! Could not reach the API. ' + error }
     }
 
 </script>
